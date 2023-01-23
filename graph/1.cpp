5414424ba24b12d564edc1cce1d1c360f6464c8a
saniyaa7 <114761701+saniyaa7@users.noreply.github.com>
2023-01-23 11:22:04 +0530

    Delete graph directory

--- graph/1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a=9,b=10;
-    cout<<"Hii eveyone saniya here!!\nwhy are you %d so sad \n"<<a+b;
-    cin<<"\nEnter some data";
-    return 0;
-}
